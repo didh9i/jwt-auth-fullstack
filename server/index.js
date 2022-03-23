e5a4671e73c3936cb97e82f384f196dd9e168c29
cclarice <179danil@gmail.com>
2022-03-23 22:52:35 +0300

    init

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParse = require('cookie-parser')
+const mongoose = require('mongoose')
+const router = require('./router/index.js')
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(cookieParse())
+app.use(cors())
+app.use('/api', router)
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
